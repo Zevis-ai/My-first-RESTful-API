b05e8bd2402f246e1534c9448213eafee6ba0c63
Zevis-ai <165157913+Zevis-ai@users.noreply.github.com>
2025-04-07 01:27:29 +0300

    Refactor server setup to use Express for handling requests

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const http = require('http');
+const app = require('./app');
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World');
-    res.end();
-});
+const server = http.createServer(app)
+
 
 server.listen(port)
