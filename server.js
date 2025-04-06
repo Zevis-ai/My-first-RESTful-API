a5b98d6e2d9aed7840d4480dee93bf0f3ae76f50
Zevis-ai <165157913+Zevis-ai@users.noreply.github.com>
2025-04-07 01:20:57 +0300

    implement basic HTTP server that responds with "Hello World"

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World');
+    res.end();
+});
+
+server.listen(port)
