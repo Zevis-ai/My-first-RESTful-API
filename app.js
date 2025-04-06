ea96538871f86e2b40d7ef2f41e9c79f656bc56c
Zevis-ai <165157913+Zevis-ai@users.noreply.github.com>
2025-04-07 02:04:39 +0300

    Add morgan middleware for logging HTTP requests

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
